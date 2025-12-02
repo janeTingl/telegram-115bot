87315c6b01fa111ca103a8c177ca160f07e75e64
HQSxcj <zy28341215@icloud.com>
2025-12-03 07:54:19 +0800

    更新 main.py

--- backend/main.py
+++ backend/main.py
@@ -457,5 +457,6 @@ if __name__ == "__main__":
         reload=reload,        # 生产环境建议关闭
         log_level="info"
     )
-    from fastapi.staticfiles import StaticFiles
+ from fastapi.staticfiles import StaticFiles
+
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
